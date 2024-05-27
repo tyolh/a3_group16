0dc74e64b0b4f4606334e492fd21a8adc0eda517
Bella0626 <oliba0626@gmail.com>
2024-05-27 14:50:40 +1000

    git commit

--- a3_group16/TicketLink/__init__.py
+++ a3_group16/TicketLink/__init__.py
@@ -1,47 +1,45 @@
-#import flask - from the package import class
-from flask import Flask 
+from flask import Flask
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+
 db = SQLAlchemy()
 
-#create a function that creates a web application
-# a web server will run this web application
 def create_app():
-  
-    app = Flask(__name__)  # this is the name of the module/package that is calling this app
-    # Should be set to false in a production environment
-    app.debug = True
-    app.secret_key = 'somesecretkey'
-    #set the app configuration data 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sitedata.sqlite'
-    #initialize db with flask app
+    app = Flask(__name__)
+
+    # we use this utility module to display forms quickly
+    Bootstrap5(app)
+
+    # A secret key for the session object
+    app.secret_key = 'somerandomvalue'
+
+    # Configue and initialise DB
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///traveldb.sqlite'
     db.init_app(app)
 
-    Bootstrap5(app)
+    #config upload folder
+    UPLOAD_FOLDER = '/static/image'
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
     
-    #initialize the login manager
+    # initialise the login manager
     login_manager = LoginManager()
-    
-    # set the name of the login function that lets user login
-    # in our case it is auth.login (blueprintname.viewfunction name)
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
     # create a user loader function takes userid and returns User
-    # Importing inside the create_app function avoids circular references
-    from .models import User
+    from .models import User  # importing here to avoid circular references
     @login_manager.user_loader
     def load_user(user_id):
-       return User.query.get(int(user_id))
-
-    #importing views module here to avoid circular references
-    # a commonly used practice.
-    from . import views
-    app.register_blueprint(views.main_bp)
-
-    from . import auth
-    app.register_blueprint(auth.auth_bp)
+        return db.session.scalar(db.select(User).where(User.id==user_id))
     
+    # add Blueprints
+    from . import views
+    app.register_blueprint(views.mainbp)
+    from . import destinations
+    app.register_blueprint(destinations.destbp)
+    from . import auth
+    app.register_blueprint(auth.authbp)
+
     return app
--- a3_group16/TicketLink/views.py
+++ a3_group16/TicketLink/views.py
@@ -1,7 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for
+from .models import Destination
+from . import db
 
-main_bp = Blueprint('main', __name__)
+mainbp = Blueprint('main', __name__)
 
-@main_bp.route('/')
+@mainbp.route('/')
 def index():
-    return '<h1>Starter code for the assessment<h1>'
+    destinations = db.session.scalars(db.select(Destination)).all()    
+    return render_template('index.html', destinations=destinations)
+
+@mainbp.route('/search')
+def search():
+    if request.args['search'] and request.args['search'] != "":
+        print(request.args['search'])
+        query = "%" + request.args['search'] + "%"
+        destinations = db.session.scalars(db.select(Destination).where(Destination.description.like(query)))
+        return render_template('index.html', destinations=destinations)
+    else:
+        return redirect(url_for('main.index'))
