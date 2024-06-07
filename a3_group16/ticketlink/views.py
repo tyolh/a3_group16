c52a976148f5d5a5bac2fc092820665f941fe059
torren03 <n10997555@qut.edu.au>
2024-06-07 19:50:37 +1000

    Update views.py

--- a3_group16/ticketlink/views.py
+++ a3_group16/ticketlink/views.py
@@ -1,1 +1,27 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from .models import Event
+from . import db
+
+mainbp = Blueprint('main', __name__)
+
+
+@mainbp.route('/')
+def index():
+    events = db.session.scalars(db.select(Event)).all()
+    return render_template('index.html', events=events)
+
+@mainbp.route('/')
+def booking():
+    events = db.session.scalars(db.select(Event)).all()
+    return render_template('events/booking.html', events=events)
+
+@mainbp.route('/search')
+def search():
+    if request.args['search'] and request.args['search'] != "":
+        print(request.args['search'])
+        query = "%" + request.args['search'] + "%"
+        events = db.session.scalars(db.select(Event)).where(Event.description.like(query))
+        return render_template('index.html', events=events)
+    else:
+        return redirect(url_for('main.index'))
 
